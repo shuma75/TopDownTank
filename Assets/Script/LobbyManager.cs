e8366cc265e1d46e17b807642bf2d7d43f9d9855
radwimps07057505 <radwimps07057505@gmail.com>
2023-08-02 22:29:41 +0900

    lap

---
+++ Assets/Script/LobbyManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField NameInput;
+    [SerializeField] Button StartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
